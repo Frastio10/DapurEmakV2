5e65297ec222f1a329fc89c1a1d91b15cc7cfbe0
frastio10 <frastio.agustiyan@gmail.com>
2019-03-23 20:44:51 +0700

    navbar transition

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -8,21 +8,31 @@ $(document).ready(function(){
 
 
 
-$(document).ready(function() {
+
   $(window).scroll(function() {
-    if ($(document).scrollTop() > 5) {
+    if ($(document).scrollTop() > 50) {
+        $(".header-wrapper").addClass("bg-white");
+        $(".header-wrapper").addClass("pt-10");
+         $(".header-wrapper").addClass("top");
+          $(".header-wrapper").addClass("bshad-thin");
        $(".scroll-animate").removeClass("none");
         $(".scroll-animate").addClass("block");
       $(".scroll-up").addClass("none");
-
-      $(".upper-nav").addClass("fixed");
-      $(".lower-nav").addClass("none");
+      $(".nav-none").addClass("none");
+      $(".header-wrapper").addClass("fixed");
+     $(".scroll-animate").removeClass("absolute");
+     $("#nav-line").addClass("none");
     } else {
       $(".scroll-up").removeClass("none");
+      $(".nav-none").removeClass("none");
+      $(".header-wrapper").removeClass("pt-10");
+       $(".header-wrapper").removeClass("top");
       $(".scroll-animate").addClass("none");
+        $(".header-wrapper").removeClass("bshad-thin");
+        $(".header-wrapper").removeClass("bg-white");
+      $("#nav-line").removeClass("none");
     }
   });
-});
 
 
 $('#vid-1').click(function(e){
