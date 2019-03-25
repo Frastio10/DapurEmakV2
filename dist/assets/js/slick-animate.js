81ad924ada51db692028704ddbf04ad362fa6bc4
frastio10 <frastio.agustiyan@gmail.com>
2019-03-26 01:50:18 +0700

    Update 3.8

--- dist/assets/js/slick-animate.js
+++ dist/assets/js/slick-animate.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('.slick').slick({
  	autoplay: true,
  	autoplaySpeed:5000,
- 	slideToShow:2,
+ 	slideToShow:1,
  	dots:true,
  	infinite:true,
  	pauseOnHover:false,
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,8 @@ mix.sass('resources/sass/style.scss', 'dist/assets/css')
    .sass('resources/sass/home.scss', 'dist/assets/css')
    .sass('resources/sass/menulist.scss', 'dist/assets/css')
    .sass('resources/sass/daftaremak.scss', 'dist/assets/css')
-   .sass('resources/sass/masakpopular.scss', 'dist/assets/css')
+   .sass('resources/sass/view-product.scss', 'dist/assets/css')
+
    .options({
     processCssUrls: false, 
     postCss: [
